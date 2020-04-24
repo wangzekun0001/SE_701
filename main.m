a7f5f15d954a569fdaa3f2216d400a5e4a6339e3
LyapunovJingci <62515845+LyapunovJingci@users.noreply.github.com>
2020-04-24 17:43:28 -0400

    Add files via upload

--- main.m
+++ main.m
@@ -1,24 +1,28 @@
 clear;clc;
 close all
 
-T = 1000;
+T = 1000;  %Time
 dt = 0.01;
-n = 100001;
+n = 100001; %这是啥
 %t = linespace(0,T,n)
 
 L = 15; %整体长度
 omega = 0:L; %等分
 
-target = [5,10,15];
+target = [5,10,15];  %这是啥
 
 s_num = 1;
 s_init = linspace(0,L,s_num);
 u_init = zeros(1,s_num);
 J1 = 0; 
-R0=zeros(1,L+1);
+
+R0=zeros(1,L+1); %measure of uncertainty at each sampling point
 R0(target+1) = 1;
-A0=zeros(1,L+1);
-A0(target+1) = 1;
+
+
+A0=zeros(1,L+1); %complexity increasing rate
+A0(target+1) = 1; %这是啥
+
 J0 = 0;
 %{
     P = s_position(omega,s,rs);
@@ -41,6 +45,8 @@ xi = 1;
 theta = defineTheta(D, 2, sigma, xi)
 gamma = defineGamma(sigma, D, 20, 2, s)
 omega = zeros(1, N);
+
+
 function f = defineD(a, b, N)
     A = zeros(1, N);
     for i = 1:N
@@ -48,6 +54,8 @@ function f = defineD(a, b, N)
     end
     f = A;          
 end
+
+
 function f = defineTheta(D, n, sigma, xi)
     if mod(xi,2)
         f = D(n) - sigma;
@@ -55,6 +63,8 @@ function f = defineTheta(D, n, sigma, xi)
         f = D(n) + sigma;
     end
 end
+
+
 function f = defineGamma(sigma, D, T, n, s)
     f = ceil(1/(2 * sigma) * (T - defineTheta(D, n, sigma, 1) + s(n)));
 end
