104b95bc3489ed940e3570a8f5a3409fd52cf834
LyapunovJingci <62515845+LyapunovJingci@users.noreply.github.com>
2020-04-24 15:33:26 -0400

    Add files via upload

--- main.m
+++ main.m
@@ -4,10 +4,22 @@ close all
 T = 1000;
 dt = 0.01;
 n = 100001;
-t = linespace(0,T,n)
+%t = linespace(0,T,n)
 
 aimlength = 15;
 omga = 0:aimlength;
 
 tar = [5];
+a = 1
+b = 2
+N = 10
+D = defineD(a, b, N)
 
+
+function f = defineD(a, b, N)
+    A = zeros(1, N);
+    for i = 1:N
+        A(i) = a + (2*i - (1/(2*N))) * (b-a);
+    end
+    f = A;          
+end
